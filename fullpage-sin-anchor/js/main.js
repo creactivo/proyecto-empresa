d6dc370cfffbdc849d199e840fd47e5fced041a7
creactivo <zonacreactiva@gmail.com>
2018-03-23 08:23:39 -0500

    pruebas

---
+++ fullpage-sin-anchor/js/main.js
@@ -0,0 +1,17 @@
+	jQuery(document).ready(function() {
+		   jQuery('#fullpage').fullpage({
+				 verticalCentered: true,
+				resize : true,
+				scrollingSpeed: 1400,
+				easing: 'easeInQuart',
+				menu: false,
+				navigation: true,
+				navigationPosition: 'right',
+				loopBottom: false,
+				loopTop: false,
+				autoScrolling: true,
+				css3: false,
+				paddingTop: 0,
+				paddingBottom: 0,
+		   });
+		});
